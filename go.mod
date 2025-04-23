3be44f82c237495b71e36a7f06e68df00a988dbd
ic_asus <chowisaac99@gmail.com>
2025-04-23 12:15:42 +0100

    http get

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ic1149/go_ldbws
+
+go 1.24.2
--- main.go
+++ main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
-	"http"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"strings"
 )
 
+// https://api1.raildata.org.uk/1010-live-departure-board-dep1_2/LDBWS/api/20220120/GetDepBoardWithDetails/RDG
+// https://api1.raildata.org.uk/1010-live-arrival-board-arr/LDBWS/api/20220120/GetArrBoardWithDetails/RDG
+// https://api1.raildata.org.uk/1010-service-details1_2/LDBWS/api/20220120/GetServiceDetails/{serviceid}
+
 func main() {
-	req, err := http.NewRequest("GET", "http://10.0.0.1/", nil)
+	const base_url string = "https://api1.raildata.org.uk/1010-live-departure-board-dep1_2/LDBWS/api/20220120/GetDepBoardWithDetails/"
+	const crs string = "RDG"
+	var url string = base_url + strings.ToUpper(crs)
+
+	req, err := http.NewRequest("GET", url, nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	req.Header.Set("x-apikey", "")
+	const dep_api_key string = ""
+	req.Header.Set("x-apikey", dep_api_key)
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", body)
+
 }
