13a207f826006ae684ba60087baead4f8d3ca8cd
ic_asus <chowisaac99@gmail.com>
2025-06-03 12:28:54 +0100

    shebang and comments

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
 cd ~
 if [ -d ./qtt]; then
-  rm -rf ./qtt
+  rm -rf ./qtt # remove existing installation
 fi
-mkdir qtt
+mkdir qtt #create temp dir
 cd qtt
 wget github.com/ic1149/quicktraintimes/releases/latest/download/quicktraintimes.tar.xz
 tar -xf quicktraintimes.tar.xz
-sudo make install
+sudo make install #install quicktraintimes
 wget github.com/ic1149/quicktraintimes/releases/latest/download/qtt.desktop
-mv qtt.desktop /usr/share/applications/qtt.desktop
+mv qtt.desktop /usr/share/applications/qtt.desktop #desktop file
 wget github.com/ic1149/quicktraintimes/releases/latest/download/qtt_icon.png
-mv qtt_icon.png /usr/share/icons/qtt_icon.png
+mv qtt_icon.png /usr/share/icons/qtt_icon.png #icon
 cd ~
-rm -rf qtt
+rm -rf qtt #remove installtion files
