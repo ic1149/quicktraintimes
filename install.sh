83ead19e85877d11694c4b11269b58910fc53db2
ic_asus <chowisaac99@gmail.com>
2025-06-03 12:26:04 +0100

    fix gh repo url in bash

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ if [ -d ./qtt]; then
 fi
 mkdir qtt
 cd qtt
-wget github.com.ic1149/quicktraintimes/releases/latest/download/quicktraintimes.tar.xz
+wget github.com/ic1149/quicktraintimes/releases/latest/download/quicktraintimes.tar.xz
 tar -xf quicktraintimes.tar.xz
 sudo make install
-wget github.com.ic1149/quicktraintimes/releases/latest/download/qtt.desktop
+wget github.com/ic1149/quicktraintimes/releases/latest/download/qtt.desktop
 mv qtt.desktop /usr/share/applications/qtt.desktop
-wget github.com.ic1149/quicktraintimes/releases/latest/download/qtt_icon.png
+wget github.com/ic1149/quicktraintimes/releases/latest/download/qtt_icon.png
 mv qtt_icon.png /usr/share/icons/qtt_icon.png
 cd ~
 rm -rf qtt
